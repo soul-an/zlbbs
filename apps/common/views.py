bb20c2ec0c331e51dff8f6626b705b82b4733196
soul-an <huanganfa66@163.com>
2019-01-15 14:52:36 +0800

    edit2

--- apps/common/views.py
+++ apps/common/views.py
@@ -49,8 +49,8 @@ def sms_captcha():
         #     # return restful.params_error(message='短信验证码发送失败！')
         #     zlcache.set(telephone, captcha)  # 这里是为了方便开发工作
         #     return restful.success()  # 为了方便开发，假设短信发送成功
-        send_sms_captcha(telephone=telephone, code=captcha)
-        return restful.success() # 为了方便开发，假设短信发送成功
+        send_sms_captcha(telephone=telephone, captcha=captcha)
+        return restful.success()  # 为了方便开发，假设短信发送成功
     else:
         return restful.params_error(message='参数错误！')
 
